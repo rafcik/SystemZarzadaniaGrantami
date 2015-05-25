35ee8d9107a764edaae095e6df42af6a808ec026
PineKnight <limbaman@gmail.com>
2015-05-25 18:30:21 +0200

    Update user.php

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -34,4 +34,22 @@ class User extends CI_Controller {
         $this->load->view('news/view', $data);
         $this->load->view('templates/footer');
     }
+    
+    public function registration()
+    {
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('user_name', 'Login', 'trim|required|min_length[4]|xss_clean');
+        $this->form_validation->set_rules('email_address', 'Email', 'trim|required|valid_email');
+        $this->form_validation->set_rules('password', 'Haslo', 'trim|required|min_length[4]|max_length[32]');
+        $this->form_validation->set_rules('con_password', 'Powtorz haslo', 'trim|required|matches[password]');
+        
+        if($this->form_validation->run() == FALSE)
+        {
+            $this-> index(); //bodaj waraca na indeks, postestuj
+        }
+        else
+        {
+            $this->user_model->add_user();
+        }
+    }
 }
