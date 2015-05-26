1333bfe4737155d348651e5370ae16abeb2fc4e6
ppulit <ppulit@gmail.com>
2015-05-26 02:57:42 +0200

    Update menu.php

--- application/views/menu.php
+++ application/views/menu.php
@@ -1,8 +1,18 @@
 <nav>
+	<div class="powiadomienie">
+		<a href="">
+			<img src="<?php echo base_url(); ?>/resources/images/notifications.png" alt="" />
+			1 powiadomienie
+		</a>
+	</div>
+	<p class="user">Użytkownik</p>
 	<ul>
-		<li><a href="#">Menu 1</a></li>
-		<li><a href="#">Menu 2</a></li>
-		<li><a href="#">Menu 3</a></li>
-		<li><a href="#">Menu 4</a></li>
+		<li><a href=""><img src="<?php echo base_url(); ?>/resources/images/calendar.png" alt="" />Kalendarz</a></li>
+		<li><br /></li>
+		<li><a href=""><img src="<?php echo base_url(); ?>/resources/images/add.png" alt="" />Dodaj grant</a></li>
+		<li><a href=""><img src="<?php echo base_url(); ?>/resources/images/edit.png" alt="" />Edytuj granty</a></li>
+		<li><br /></li>
+		<li><a href="index.php?/auth/logout"><img src="<?php echo base_url(); ?>/resources/images/logout.png" alt="" />Wyloguj</a></li>
 	</ul>
 </nav>
+<div class="tresc">
