6cd84cb79508c1b98b667920267fa6f0d7f057bb
ppulit <ppulit@gmail.com>
2015-05-26 03:17:37 +0200

    Update main_content.php

--- application/views/main_content.php
+++ application/views/main_content.php
@@ -1,10 +1,10 @@
-Witamy w systemie zarządzania grantami
-<br>
+<h1>Witamy w systemie zarządzania grantami!</h1>
+<h2>
 <?php
 	if($logged_in) {
 		echo "Jesteś zalogowany jako {$logged_in['imie']} {$logged_in['nazwisko']}. <a href='index.php?/auth/logout'>Wyloguj</a>";
 	} else {
-		echo 'Jesteś nie zalogowany. Przejdź na stronę <a href="index.php?/auth/">logowania</a>';
+		echo 'Nie jesteś zalogowany :(<br />Aby zacząć korzystać z naszego super systemu, przejdź na stronę <a href="index.php?/auth/">logowania</a>.';
 	}
 ?>
-
+</h2>
