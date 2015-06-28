eafe285e115d231915c61db0b30dfaed99f24d88
ppulit <ppulit@gmail.com>
2015-06-28 04:05:05 +0200

    Update create.php

--- application/views/Grant/create.php
+++ application/views/Grant/create.php
@@ -1,33 +1,20 @@
+<h1>Dodwanie nowego granta</h1>
 <form id="create_grant"  method="post" action="insert">
-    <div class="form_description">
-        <h2>Dodwanie nowego granta</h2>
-    </div>
-
-
-            <label class="description" for="nazwa">Nazwa* </label>
-            <div>
-                <input id="nazwa" name="nazwa" type="text" maxlength="255" value="" required="To pole jest obowiążkowe"/>
-            </div>
-
-        <label class="opis" for="opis">Opis </label>
-        <div>
-            <textarea id="opis" name="opis"  maxlength="500" value="" > </textarea>
-        </div>
-
-        </li>
-
-        <label for="kategoria">Kategoria </label>
-        <div>
-            <select id="kategoria_select" name="kategoria_select" onchange="updateId()">
-                <?php
-                    foreach($kat as $k) {
-                    echo '<option id="' . $k->id . '" value="' . $k->id . '">'  . $k->nazwa . '</option>' . "\n";
-                }
-                ?>
-            </select>
-        </div>
-        </li>
-
+    <label class="description" for="nazwa">Nazwa* </label>
+    <input id="nazwa" name="nazwa" type="text" maxlength="255" value="" required="To pole jest obowiążkowe"/>
+	<br />
+    <label class="opis" for="opis">Opis </label>
+    <textarea id="opis" name="opis"  maxlength="500" value="" > </textarea>
+	<br />
+    <label for="kategoria">Kategoria </label>
+    <select id="kategoria_select" name="kategoria_select" onchange="updateId()">
+        <?php
+            foreach($kat as $k) {
+            echo '<option id="' . $k->id . '" value="' . $k->id . '">'  . $k->nazwa . '</option>' . "\n";
+            }
+        ?>
+    </select>
+    
     <input type="text" id="kategoria_id" value="1" style="display: none;" />
 
     <script>
@@ -36,30 +23,29 @@
             $('#kategoria_id').val(katId);
         }
     </script>
-
+	<br />
 
         <label  for="budzet">Budżet (zł)*</label>
-        <div>
-            <input id="budzet" name="budzet" type="number" maxlength="255" step="any" min="0" value="" required="To pole jest obowiążkowe"/>
-        </div>
 
+            <input id="budzet" name="budzet" type="number" maxlength="255" step="any" min="0" value="" required="To pole jest obowiążkowe"/>
+<br />
 
         <label  for="czasRozpoczecia">Czas rozpoczęcia* </label>
-        <div>
+      
             <input id="czasRozpoczecia" name="czasRozpoczecia" type="date" value="<?php echo date('Y-m-d');?>" required="To pole jest obowiążkowe"/>
-        </div>
+     <br />
 
 
         <label for="czasZakonczenia">Czas zakończenia* </label>
-        <div>
+        
             <input id="czasZakonczenia" name="czasZakonczenia" type="date" value="<?php echo date('Y-m-d', strtotime(date('Y-m-d').' + 140 days'));?>" required="To pole jest obowiążkowe"/>
-        </div>
-
+        
+<br />
 
         <label for="czasRozliczenia">Czas rozliczenia (liczba tygodni)* </label>
-        <div>
+        
             <input id="czasRozliczenia" name="czasRozliczenia" type="number"  value="20" required="To pole jest obowiążkowe"/>
-        </div>
+       <br /><br />
 
             <input type="hidden" name="form_id" value="1014494" />
             <input id="saveForm" class="button_text" type="submit" name="submit" value="Dodaj" />
