24532183543b7845248bb965499bcefc97faa4e9
ppulit <ppulit@gmail.com>
2015-06-29 23:10:22 +0200

    Update newtab.php

--- application/views/Grant/newtab.php
+++ application/views/Grant/newtab.php
@@ -1,36 +1,25 @@
 <?php
 require_once('tabs_layout.php');
 ?>
-<br />
-<br />
-<br />
 <h3 id="tabName">Dodawanie nowej zakładki</h3>
 
+<form id="create_tab" method="post" action="http://granty.lyrmet.pl/grant/insert_tab">
+    <h1>Dodwanie nowej zakładki</h1>
+    <label class="description" for="nazwa">Nazwa <span style="color:red">*</span> </label>
+        <input id="nazwa" name="nazwa" type="text" maxlength="255" value="" required="To pole jest obowiązkowe"/>
 
-<form id="create_tab"  method="post" action="http://granty.lyrmet.pl/grant/insert_tab">
-    <div class="form_description">
-        <h2>Dodwanie nowej zakładki</h2>
-    </div>
-        <label class="description" for="nazwa">Nazwa* </label>
-        <div>
-            <input id="nazwa" name="nazwa" type="text" maxlength="255" value="" required="To pole jest obowiązkowe"/>
-        </div>
+    <label class="opis" for="opis">Opis&nbsp; &nbsp;</label>
+        <textarea id="opis" name="opis"  maxlength="500" value="" > </textarea>
 
-        <label class="opis" for="opis">Opis </label>
-        <div>
-            <textarea id="opis" name="opis"  maxlength="500" value="" > </textarea>
-        </div>
-
-        <label for="owner">Właściciel zakładki</label>
-        <div>
-            <select id="owner_select" name="owner_select">
-                <?php
-                    foreach($Users as $u) {
-                        echo '<option id="' . $u->id . '" value="' . $u->id . '">'  . $u->imie . ' ' . $u->nazwisko . '</option>' . "\n";
-                    }
-                ?>
-        </select>
-        </div>
+    <label for="owner">Właściciel zakładki&nbsp; &nbsp;</label>
+        <select id="owner_select" name="owner_select">
+        <?php
+            foreach($Users as $u) {
+				echo '<option id="' . $u->id . '" value="' . $u->id . '">'  . $u->imie . ' ' . $u->nazwisko . '</option>' . "\n";
+            }
+        ?>
+		</select>
+ <br /><br /><br />
     <input type="hidden" name="form_id" value="1015594" />
     <input type="hidden" name="idGrant" value="<?php echo $idGrant; ?>" />
     <input id="saveForm" class="button_text" type="submit" name="submit" value="Dodaj" />
