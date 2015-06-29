95cb25a631b8d9f531a84c9e62630f5a8dd04134
ppulit <ppulit@gmail.com>
2015-06-29 21:16:15 +0200

    Update create.php

--- application/views/Grant/create.php
+++ application/views/Grant/create.php
@@ -1,12 +1,12 @@
 <h1>Dodwanie nowego granta</h1>
 <form id="create_grant"  method="post" action="insert">
-    <label class="description" for="nazwa">Nazwa* </label>
+    <label class="description" for="nazwa">Nazwa <span style="color:red">*</span></label>
     <input id="nazwa" name="nazwa" type="text" maxlength="255" value="" required="To pole jest obowiążkowe"/>
 	<br />
-    <label class="opis" for="opis">Opis </label>
+    <label class="opis" for="opis">Opis&nbsp; &nbsp;</label>
     <textarea id="opis" name="opis"  maxlength="500" value="" > </textarea>
 	<br />
-    <label for="kategoria">Kategoria </label>
+    <label for="kategoria">Kategoria <span style="color:red">*</span></label>
     <select id="kategoria_select" name="kategoria_select" onchange="updateId()">
         <?php
             foreach($kat as $k) {
@@ -25,24 +25,24 @@
     </script>
 	<br />
 
-        <label  for="budzet">Budżet (zł)*</label>
+        <label  for="budzet">Budżet (zł) <span style="color:red">*</span></label>
 
             <input id="budzet" name="budzet" type="number" maxlength="255" step="any" min="0" value="" required="To pole jest obowiążkowe"/>
 <br />
 
-        <label  for="czasRozpoczecia">Czas rozpoczęcia* </label>
+        <label  for="czasRozpoczecia">Data rozpoczęcia <span style="color:red">*</span></label>
       
             <input id="czasRozpoczecia" name="czasRozpoczecia" type="date" value="<?php echo date('Y-m-d');?>" required="To pole jest obowiążkowe"/>
      <br />
 
 
-        <label for="czasZakonczenia">Czas zakończenia* </label>
+        <label for="czasZakonczenia">Data zakończenia <span style="color:red">*</span></label>
         
             <input id="czasZakonczenia" name="czasZakonczenia" type="date" value="<?php echo date('Y-m-d', strtotime(date('Y-m-d').' + 140 days'));?>" required="To pole jest obowiążkowe"/>
         
 <br />
 
-        <label for="czasRozliczenia">Czas rozliczenia (liczba tygodni)* </label>
+        <label for="czasRozliczenia">Czas rozliczenia (liczba tygodni) <span style="color:red">*</span></label>
         
             <input id="czasRozliczenia" name="czasRozliczenia" type="number"  value="20" required="To pole jest obowiążkowe"/>
        <br /><br />
